534523ea7905bb3d1303290caac840e0921045ac
Savas07 <savas_007_2000@hotmail.com>
2022-04-04 16:22:39 +0300

    küçük fix

--- entity/GradientView.swift
+++ entity/GradientView.swift
@@ -15,10 +15,7 @@ import UIKit
 
         override func draw(_ rect: CGRect) {
             let gradient: CAGradientLayer = CAGradientLayer()
-            gradient.frame = CGRect(x: CGFloat(0),
-                                    y: CGFloat(0),
-                                    width: superview!.frame.size.width,
-                                    height: superview!.frame.size.height)
+            gradient.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: superview!.frame.size.width, height: superview!.frame.size.height)
             gradient.colors = [startColor.cgColor, endColor.cgColor]
             gradient.zPosition = -1
             layer.addSublayer(gradient)
